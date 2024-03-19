8b3d657deede650cd78058e5647c523badb145f9
Naimul02 <naimulislum39@gmail.com>
2024-03-19 23:40:30 +0600

    added users data

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,10 +1,20 @@
 import React from "react";
-
+import { useLoaderData } from "react-router-dom";
+import User from "../User/User";
+import "./Users.css";
 const Users = () => {
+  // useEffect dile tomar jokhon component a asteche tar por a giye data load er cesta kore ebong er pore ase. ar tumi jodi useLoaderData use koro tahole se route a asar age data ta k load kore niye ase
+  const users = useLoaderData();
+  console.log(users);
   return (
     <div>
-      <h2>Our Users</h2>
+      <h2>Our Users : {users.length}</h2>
       <p>Fantastic and vodro users</p>
+      <div className="users">
+        {users.map((user) => (
+          <User key={user.id} user={user}></User>
+        ))}
+      </div>
     </div>
   );
 };
