26b40162482fa44fa4582946c3f94aa65e6a77ba
Naimul02 <naimulislum39@gmail.com>
2024-03-20 10:41:28 +0600

    dynamic single data load

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import "./App.css";
 import Users from "./components/Users/Users";
+import UserDetails from "./components/UsersDetails/UserDetails";
 
 const router = createBrowserRouter([
   {
@@ -26,6 +27,14 @@ const router = createBrowserRouter([
         path: "/users",
         loader: () => fetch("https://jsonplaceholder.typicode.com/users"),
         element: <Users></Users>,
+      },
+      {
+        // :userId eta kono static ba fix text na eta ek rokom variable jekhane bibhinno man aste pare .for example 1 ,2, 3 , 4....etc.jodi ase tahole tumi <UserDetails></UserDetails>Component e niye jaba
+        path: "/user/:userId",
+        // params er moddhe object akare userId ta paba jeta diye specefic data k peta paro.({params}) => console.log(params),
+        loader: ({ params }) =>
+          fetch(`https://jsonplaceholder.typicode.com/users/${params.userId}`),
+        element: <UserDetails></UserDetails>,
       },
     ],
   },
